cb1f3e0ebb572bd428753da2d5d68e835d9bf404
Anser Shafiq <AnserShafiq>
2023-07-17 13:13:33 +0500

    Updated On 17/07/2023

--- client/src/components/customers/buildingModels/page.js
+++ client/src/components/customers/buildingModels/page.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header1 from '../../Header/header.js';
 import Footer from '../../Header/footer.js';
-import house1 from '../../../images/3M2F.PNG';
+import house1 from '../../../images/3M2F.jpeg';
 import house2 from '../../../images/3M1F.PNG';
+import house3 from '../../../images/7M1F.jpg';
+import house4 from '../../../images/7M2F.jpeg';
+import house5 from '../../../images/1K1F.webp';
+import house6 from '../../../images/1K2F.jpeg';
+import house7 from '../../../images/2K1F.jpg';
+import house8 from '../../../images/2K2F.jpg';
 
 import './styles.css';
 
@@ -33,11 +39,11 @@ const BuildingModels = () =>{
                 <h3 className='divHeading'>For 7 & 10 Marla House:</h3>
                 <div className='innerDiv'>
                     <div className='cardModel'>
-                        <img alt='3MarlaDoubleStory' width={400} height={300} src={house1} className='modelsImg'/>
+                        <img alt='3MarlaDoubleStory' width={400} height={300} src={house4} className='modelsImg'/>
                         <h3 className='modelName'>Double Story</h3>
                     </div>
                     <div className='cardModel'>
-                        <img alt='3MarlaSingleStory' width={400} height={300} src={house2} className='modelsImg'/>
+                        <img alt='3MarlaSingleStory' width={400} height={300} src={house3} className='modelsImg'/>
                         <h3 className='modelName'>Single Story</h3>
                     </div>
                 </div>
@@ -46,11 +52,11 @@ const BuildingModels = () =>{
                 <h3 className='divHeading'>For 13 Marla & 1 Kanal House:</h3>
                 <div className='innerDiv'>
                     <div className='cardModel'>
-                        <img alt='3MarlaDoubleStory' width={400} height={300} src={house1} className='modelsImg'/>
+                        <img alt='3MarlaDoubleStory' width={400} height={300} src={house6} className='modelsImg'/>
                         <h3 className='modelName'>Double Story</h3>
                     </div>
                     <div className='cardModel'>
-                        <img alt='3MarlaSingleStory' width={400} height={300} src={house2} className='modelsImg'/>
+                        <img alt='3MarlaSingleStory' width={400} height={300} src={house5} className='modelsImg'/>
                         <h3 className='modelName'>Single Story</h3>
                     </div>
                 </div>
@@ -59,11 +65,11 @@ const BuildingModels = () =>{
                 <h3 className='divHeading'>For 2 Kanal House:</h3>
                 <div className='innerDiv'>
                     <div className='cardModel'>
-                        <img alt='3MarlaDoubleStory' width={400} height={300} src={house1} className='modelsImg'/>
+                        <img alt='3MarlaDoubleStory' width={400} height={300} src={house8} className='modelsImg'/>
                         <h3 className='modelName'>Double Story</h3>
                     </div>
                     <div className='cardModel'>
-                        <img alt='3MarlaSingleStory' width={400} height={300} src={house2} className='modelsImg'/>
+                        <img alt='3MarlaSingleStory' width={400} height={300} src={house7} className='modelsImg'/>
                         <h3 className='modelName'>Single Story</h3>
                     </div>
                 </div>
